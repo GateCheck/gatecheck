d52d6daeb77b6b1bb41de4189af7f042952775e5
Heknon <heknonplayz@gmail.com>
2020-03-15 12:21:13 +0200

    refactored code. removed real life id from results

--- api/routes/students/students.js
+++ api/routes/students/students.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { Student } = require('../../models/index');
+const {
+    Student
+} = require('../../models/index');
 
 router.get("/students", async (req, res) => {
     const students = [];
     for (const studentDoc of await Student.find()) {
         const student = studentDoc.toJSON();
         delete student.password;
+        delete student.id_number;
         students.push(student);
     }
     res.json(students);
