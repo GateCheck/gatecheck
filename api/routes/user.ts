dd9d35915922c7fd8eeb586e890b700f5e6ffad0
Heknon <development@heknon.com>
2020-03-22 11:11:22 +0200

    added generic user data get route

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,9 +1,22 @@
-import express from 'express';
+import express, { Response } from 'express';
 const router = express.Router();
 
 import getAuthenticatedUser from '../middleware/get-authenticated-user'; // Must be an admin to delete an account or the user being deleted. ensures so.
 
 import UserController from '../controllers/user_management';
+import { AuthenticatedRequest, IUser } from '../..';
+import { removeConfidentialData } from '../utils';
+
+router.get(
+	'/',
+	getAuthenticatedUser as any,
+	((req: AuthenticatedRequest<IUser>, res: Response) => {
+		res.status(200).json({
+			sucess: true,
+			user: removeConfidentialData(req.user, true)
+		});
+	}) as any
+);
 
 router.post('/signup', UserController.signup);
 
